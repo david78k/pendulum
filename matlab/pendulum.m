36b8aeba130d834ceead9f49c5b3c637a4ffba8c
david78k <david78k@gmail.com>
2014-07-25 23:52:02 -0400

    only one figure generated

--- matlab/pendulum.m
+++ matlab/pendulum.m
@@ -20,7 +20,7 @@ t=0:0.01:10;
 impulse(T,t)
 title('Response of Pendulum Position to an Impulse Disturbance under PID Control: Kp = 1, Ki = 1, Kd = 1');
 
-figure;
+% figure;
 Kp = 100;
 Ki = 1;
 Kd = 1;
@@ -31,13 +31,13 @@ impulse(T,t)
 axis([0, 2.5, -0.2, 0.2]);
 title('Response of Pendulum Position to an Impulse Disturbance under PID Control: Kp = 100, Ki = 1, Kd = 1');
 
-figure;
+% figure;
 Kp = 100;
 Ki = 1;
 Kd = 20;
 C = pid(Kp,Ki,Kd);
 T = feedback(P_pend,C);
 t=0:0.01:10;
-impulse(T,t)
-axis([0, 2.5, -0.2, 0.2]);
-title('Response of Pendulum Position to an Impulse Disturbance under PID Control: Kp = 100, Ki = 1, Kd = 20');
+% impulse(T,t)
+% axis([0, 2.5, -0.2, 0.2]);
+% title('Response of Pendulum Position to an Impulse Disturbance under PID Control: Kp = 100, Ki = 1, Kd = 20');
