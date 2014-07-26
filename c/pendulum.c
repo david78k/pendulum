fbee4810f3f169cfe11eabd94a681063b1513900
Tae Seung Kang <tsk@lin114-04.cise.ufl.edu>
2014-07-25 22:14:07 -0400

    all

--- c/pendulum.c
+++ c/pendulum.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
-
 void main () {
 	printf("pendulum PI = %f\n", M_PI);
 
+	// alpha > 1, beta > 0
+	// beta^2 > 4(alpha - 1)
 	double theta, alpha, beta, phi, t, ut;
+	// first derivative, second derivative
+	double phi_1, phi_2;
 		
+	// initialize variables
 	t = 0;
+	alpha = 1.1;
+	beta = 1;
+	
+	// phi_2 + sin = u
+
 	while (t < 10) {
+		//phi_2 + beta*phi_1 + (alpha - 1)*phi = 0;
 		printf("%1.0f\n", t);
 		t += 1;
 	}
