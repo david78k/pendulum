6d05b3f87851b6eb07e5b8edad72f3d084799453
Tae Seung Kang <tsk@lin114-04.cise.ufl.edu>
2014-07-25 21:57:01 -0400

    all

--- c/pendulum.c
+++ c/pendulum.c
@@ -1,5 +1,17 @@
+#include <stdio.h>
+#include <math.h>
 
 
 void main () {
+	printf("pendulum PI = %f\n", M_PI);
 
+	double theta, alpha, beta, phi, t, ut;
+		
+	t = 0;
+	while (t < 10) {
+		printf("%1.0f\n", t);
+		t += 1;
+	}
 }
+
+
