1ee7eec7a8e8db2e1e531539d0b69c7039c103f0
david78k <david78k@gmail.com>
2014-07-26 10:51:09 -0400

    comments

--- matlab/pendulum.m
+++ matlab/pendulum.m
@@ -1,10 +1,10 @@
 % system structure
-M = 0.5;
-m = 0.2;
-b = 0.1;
-I = 0.006;
-g = 9.8;
-l = 0.3;
+M = 0.5; % cart mass
+m = 0.2; % pendulum mass
+b = 0.1; % coefficient of friction for cart
+I = 0.006; % mass moment of itertia of the pendulum
+g = 9.8; % gravity
+l = 0.3; % length to pendulum center of mass
 q = (M+m)*(I+m*l^2)-(m*l)^2;
 s = tf('s');
 P_pend = (m*l*s/q)/(s^3 + (b*(I + m*l^2))*s^2/q - ((M + m)*m*g*l)*s/q - b*m*g*l/q);
@@ -15,12 +15,11 @@ Ki = 1;
 Kd = 1;
 C = pid(Kp,Ki,Kd);
 T = feedback(P_pend,C);
-
 t=0:0.01:10;
 impulse(T,t)
 title('Response of Pendulum Position to an Impulse Disturbance under PID Control: Kp = 1, Ki = 1, Kd = 1');
 
-% figure;
+figure;
 Kp = 100;
 Ki = 1;
 Kd = 1;
