432b1e850f7c5f4e9a09b0ccd8d6b325be3ddf4f
david78k <david78k@gmail.com>
2015-01-21 22:24:29 -0500

    rl_pole_nn single layer

---
+++ matlab/rl_pole_nn/NN_update.m
@@ -0,0 +1,22 @@
+function [w, v, e, xbar] = NN_update(N_BOXES, LAMBDAw, LAMBDAv, ALPHA, BETA, w, v, e, xbar, rhat, failed)
+
+  for i=1:N_BOXES,
+        %Update all weights.
+         w(i) = w(i)+ ALPHA * rhat * e(i);
+	     v(i) = v(i)+ BETA * rhat * xbar(i);
+         
+         if (v(i) < -1.0)
+	        v(i) = v(i);
+         end
+         
+         if (failed)
+             %If failure, zero all traces.
+              e(i) = 0.;
+	          xbar(i) = 0.;
+              
+          else
+              e(i)=e(i) * LAMBDAw;
+              xbar(i) =xbar(i)* LAMBDAv;
+          end
+          
+      end
