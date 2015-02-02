bacd2c45aca1e41a06d528af510a0fd66bafc6e7
david78k <david78k@gmail.com>
2015-02-02 01:10:34 -0500

    max 30k steps

--- matlab/rl_pole/nn_two_outputs_action_network_GUI_force/getForce.m
+++ matlab/rl_pole/nn_two_outputs_action_network_GUI_force/getForce.m
@@ -3,12 +3,13 @@ function [f] = getForce (push, t, tau)
 fmax = 600;
 % fmax = 1;
 
-if push == 1
-    f = fmax*t*exp(-t/tau);
-%     f = fmax*t*exp(-t);
-elseif push == -1
-    f = fmax*-t*exp(-t/tau);
-%     f = fmax*-t*exp(-t);
-else
-    f = 0;
-end
+f = push*fmax*t*exp(-t/tau);
+% if push == 1
+%     f = fmax*t*exp(-t/tau);
+% %     f = fmax*t*exp(-t);
+% elseif push == -1
+%     f = fmax*-t*exp(-t/tau);
+% %     f = fmax*-t*exp(-t);
+% else
+%     f = 0;
+% end
