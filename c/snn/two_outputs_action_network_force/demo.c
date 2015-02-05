9c1ecf2804580e74e11422ee679d66e44a48bd7f
Tae Seung Kang <tsk@coconut.cise.ufl.edu>
2015-02-05 00:15:16 -0500

    weights

--- c/snn/two_outputs_action_network_force/demo.c
+++ c/snn/two_outputs_action_network_force/demo.c
@@ -10,6 +10,8 @@ int i;
 
 void cartpole_snn();
 void cartpole();
+void initWeights();
+void updateWeights();
 
 int main() {
 	#pragma omp parallel
@@ -280,6 +282,55 @@ if (abs(x) > max_pos || abs(theta) > max_angle)
     failure = -1; //to signal failure 
 else
     failure = 0;
+end
+*/
+}
+
+void updateWeights() {
+/*
+function [a,b,c,d,e,f] = updateWeights(BETA, RHO, BETAH, RHOH, rhat, ...
+    unusualness, xold, yold, a, b, c, d, e, f, z)
+
+for i = 1: 5,  
+     % for each hidden unit
+    factor1 = BETAH * rhat * yold(i) * (1 - yold(i)) * sign(c(i));
+    factor2 = RHOH * rhat * z(i) * (1 - z(i)) * unusualness;% * sign(f(i)); % unusualness required, sign(f(i)) optional
+    for j = 1:5,    
+        % for each weight I-H
+        a(i,j) = a(i, j) + factor1 * xold(j); % evaluation network I-H
+        d(i,j) = d(i, j) + factor2 * xold(j); % action network I-H
+    end
+    % for each weight H-O
+    b(i) = b(i) + BETA * rhat * xold(i);   % evaluation network I-O
+    c(i) = c(i) + BETA * rhat * yold(i);   % evaluation network H-O
+
+    for j = 1:2
+        e(i,j) = e(i,j) + RHO * rhat * xold(i) * unusualness;  % action network I-O
+        f(i,j) = f(i,j) + RHO * rhat * z(i) * unusualness;  % action network H-O
+    end
+%     e(i) = e(i) + RHO * rhat * xold(i) * unusualness;  % action network I-O
+%     f(i) = f(i) + RHO * rhat * z(i) * unusualness;  % action network H-O
+end
+*/
+}
+
+void initWeights() {
+/*
+function [a,b,c,d,e,f] = init_weights()
+% Initialize action and heuristic critic weights and traces 
+% to random values between -0.1 and 0.1
+for i = 1: 5,
+    for j = 1:5,
+        a(i,j) = randomdef * 0.2 - 0.1; % evaluation network I-H
+        d(i,j) = randomdef * 0.2 - 0.1; % action network I-H
+    end
+%     b(i) = randomdef;   % evaluation network I-O
+    b(i) = randomdef * 0.2 - 0.1;   % evaluation network I-O
+    c(i) = randomdef * 0.2 - 0.1;   % evaluation network H-O
+    for j = 1:2
+        e(i,j) = randomdef * 0.2 - 0.1;   % action network I-O
+        f(i,j) = randomdef * 0.2 - 0.1;   % action network H-O
+    end
 end
 */
 }
