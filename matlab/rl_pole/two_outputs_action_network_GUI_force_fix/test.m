5f484fd00a8b807a726c408d0a58e6aee502a8bd
david78k <david78k@gmail.com>
2015-02-07 13:57:51 -0500

    cont force dt=0.01, left=0 bug

---
+++ matlab/rl_pole/two_outputs_action_network_GUI_force_fix/test.m
@@ -0,0 +1,14 @@
+function test
+
+% clear all;
+
+xpoints = [];
+ypoints = [];
+
+for tv = 1:5,
+    subplot(2,1,2);
+    xpoints(tv) = tv;
+    ypoints(tv) = tv;
+    plot(xpoints,ypoints)
+    % disp(['success'])
+end
