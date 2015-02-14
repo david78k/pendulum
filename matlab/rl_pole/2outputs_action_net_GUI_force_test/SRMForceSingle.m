9c4e64f630aa3e4db43959ca3636f1f8d8836c93
david78k <david78k@gmail.com>
2015-02-13 20:13:12 -0500

    tau0.02 dt-0.001

--- matlab/rl_pole/2outputs_action_net_GUI_force_test/SRMForceSingle.m
+++ matlab/rl_pole/2outputs_action_net_GUI_force_test/SRMForceSingle.m
@@ -4,19 +4,17 @@ clear all;
 
 dt = 0.02; % 20ms, Fmax = 0.3679, period = 9.18sec 460steps
 % dt = 0.001; % 1ms, Fmax = 0.36788, period = 9.199sec, 9200steps
+dt = 0.001; % 1ms, Fmax = 0.0073576, period = 0.179sec, 180steps
 tau = 1; % 
-% tau = 0.5; % [-3, 3]
-% tau = 0.02; % [-0.018, 0.018], fmax = 1
-% tau = 0.02; % [-0.05, 0.05], fmax = 3
-% tau = 0.02; % [0.09, 0.09], fmax = 5
-% tau = 0.02; % [-0.18, 0.18], fmax = 10
-% tau = 0.02; % [-10, 10], fmax = 600
+tau = 0.5; % Fmax = 0.18394, period = 4.38sec, 220steps
+tau = 0.02; % Fmax = 0.0073576, period = 0.18sec, 10steps
 
 steps = []; F = []; push = [];
 
 tic
 % for step = 1:9200
-for step = 1:460
+% for step = 1:460
+for step = 1:180
     steps(step) = (step - 1)*dt;
     t = steps(step);
     F(step) = t*exp(-t/tau);
