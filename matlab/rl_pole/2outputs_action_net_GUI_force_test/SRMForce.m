732520f986c91e9f345c00ff1429af02d11194bf
david78k <david78k@gmail.com>
2015-02-13 22:01:41 -0500

    180k last 400 spikes

--- matlab/rl_pole/2outputs_action_net_GUI_force_test/SRMForce.m
+++ matlab/rl_pole/2outputs_action_net_GUI_force_test/SRMForce.m
@@ -1,36 +1,38 @@
 % function SRMForce
-clf;
-clear all;
+clf; clear all;
 
-sampleTime = 0.02;
-% tau = 1; % [-5, 7]
+% F = [-8, 8]
+fmax = 1;
+dt = 0.02;
+tau = 1; % [-10, 10] for (fmax = 1, dt = 0.02)
 % tau = 0.5; % [-3, 3]
-tau = 0.02; % [-0.018, 0.018], fmax = 1
+% tau = 0.02; % [-0.018, 0.018], fmax = 1
 % tau = 0.02; % [-0.05, 0.05], fmax = 3
 % tau = 0.02; % [0.09, 0.09], fmax = 5
 % tau = 0.02; % [-0.18, 0.18], fmax = 10
 % tau = 0.02; % [-10, 10], fmax = 600
 
-steps = [];
-F = [];
-push = [];
+steps = []; F = []; push = []; f = [];
 
 tic
 
-for step = 1:6000,
-    steps(step) = (step - 1)*sampleTime;
-%     ypoints(i) = (i + 100 )*tau; % old 100 steps before
-%     fpoints(i) = (i + 200)*tau;  % old 200 steps past
-    F(step) = 0;
+for step = 1:180000
+    steps(step) = (step - 1)*dt;
     push(step) = mod(randi(3),3) - 1;
-    for k = 1:step
-        F(step) = F(step) + getForce(push(k), (step - k)*sampleTime, tau);
+%     f(step) = fmax * push(step) * t * exp(-t/tau);
+    sum = 0;
+%     for k = 1:step
+    upto = min(step, 500);
+    for k = 1:upto
+%         k
+        t = k*dt;
+%         t = (step - k)*dt;
+        sum = sum + fmax * push(step + 1 - k) * t * exp(-t/tau);
+%         F(step) = F(step) + getForce(push(k), (step - k)*sampleTime, tau);
 %         F(step) = F(step) + getForce(push(k), (step - k)*tau);
+%         disp(['k = ' num2str(k) ' sum = ' num2str(sum)]);
     end
-    
-    %     subplot(2,1,2);
-%     plot(xpoints,ypoints)
-    % disp(['success'])
+    F(step) = sum;
 end
 
 hold on;
