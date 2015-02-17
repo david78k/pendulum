5f067fae8abe0ffcfd24ce3262e984d8364d0613
Tae Seung Kang <tsk@cyclone.cise.ufl.edu>
2015-02-17 12:15:55 -0500

    gnuplot-py

--- c/pole/0.5.5-plot/gnuplot.c
+++ c/pole/0.5.5-plot/gnuplot.c
@@ -14,15 +14,49 @@ int main(int argc, char **argv)
  
         fprintf(gp, "set terminal png\n");
         fprintf(gp, "set output 'gnuplot.png'\n");
-        //fprintf(gp, "set samples 2000\n");
-        fprintf(gp, "plot abs(sin(x))\n");
-        fprintf(gp, "rep abs(cos(x))\n");
+        //fprintf(gp, "set samples 2000\n"); // optional
+        //fprintf(gp, "plot abs(sin(x))\n");
+        //fprintf(gp, "rep abs(cos(x))\n");
 
 // read data from file
 // 180k-fm200-sup1-sample1-r1.train
+	FILE *file; // = fopen(fname);
+	char *fname = "180k-fm200-sup1-sample1-r1.train";
+ if ((file = fopen(fname,"r")) == NULL) {
+    printf("Couldn't open %s\n",fname);
+    return;
+  }
 
+	printf("file %s opened\n", fname);
+
+	int i, j;
+	int left[180000];
+	int right[180000];
+	float rhat[180000][2];
+	float h[180000];
+	float hdot[180000];
+	float theta[180000];
+	float thetadot[180000];
+	float force[180000];
 // left, right, r_hat[0], r_hat[1], h, h_dot, theta, theta_dot, force 
 // 0 0 0.0113 0.0113 -0.0755 -0.0499 0.9063 0.6871 0.0000
+
+//  for(i = 0; i < 5; i++)
+	for(j = 0; j < 10; j++) {
+        	fscanf(file,"%d",&left[j]);
+        	fscanf(file,"%d",&right[j]);
+        	fscanf(file,"%f",&rhat[j][0]);
+        	fscanf(file,"%f",&rhat[j][1]);
+        	fscanf(file,"%f",&h[j]);
+        	fscanf(file,"%f",&hdot[j]);
+        	fscanf(file,"%f",&theta[j]);
+        	fscanf(file,"%f",&thetadot[j]);
+        	fscanf(file,"%f",&force[j]);
+		//printf("%d %d %f %f %f\n", left[j], right[j], rhat[j][0], rhat[j][1], h[j]);
+		printf("%d %d %.4f %.4f %.4f %.4f %.4f %.4f %.4f\n", left[j], right[j], rhat[j][0], rhat[j][1]
+	//		, h[j], hdot[j], theta[j]);
+			, h[j], hdot[j], theta[j], thetadot[j], force[j]);
+	}
 
         fclose(gp);
  
