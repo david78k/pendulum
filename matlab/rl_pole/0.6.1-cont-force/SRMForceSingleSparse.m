b44d95f1fb9756e913e9f9e69dfcde06ef6fe8b9
david78k <david78k@gmail.com>
2015-02-26 20:42:28 -0500

    sparse force

--- matlab/rl_pole/0.6.1-cont-force/SRMForceSingleSparse.m
+++ matlab/rl_pole/0.6.1-cont-force/SRMForceSingleSparse.m
@@ -1,7 +1,9 @@
 % function SRMForce
 clf; clear all;
 
-fmax = 1000;
+fmax = 1000; % F(-7.3576, 7.3576)
+% fmax = 2000; % F(-14.7152, 14.7152)
+% fmax = 10000; % F(-73.5759, 73.5759)
 % dt = 0.02; % 20ms, tau = 1, Fmax = 0.3679, period = 9.18sec 460steps (300-400 ok)
 dt = 0.001; % 1ms, tau = 1, Fmax = 0.36788, period = 9.199sec, 9200steps
 % dt = 0.001; % 1ms, tau = 0.02, Fmax = 0.0073576, period = 0.179sec, 180steps
@@ -9,23 +11,23 @@ dt = 0.001; % 1ms, tau = 1, Fmax = 0.36788, period = 9.199sec, 9200steps
 % tau = 0.5; % dt = 0.02, Fmax = 0.18394, period = 4.38sec, 220steps
 tau = 0.02; % dt = 0.001, Fmax = 0.0073576, period = 0.18sec, 10steps
 last_steps = 200; % 460, 8000, 9200
-total_steps = 10000; % 1sec
+total_steps = 2000; % 1sec
 
 steps = []; F = []; push = [];
 
 tic
-for step = 1:(total_steps )
+for step = 1:(total_steps)
 %     step
 %     endpoint = min(last_steps, step + last_steps);
     startpoint = mod(step, last_steps);
-    endpoint = startpoint + last_steps;
+    endpoint = startpoint + last_steps - 1;
     if startpoint == 1   
-        push(step) = mod(randi(3),3) - 1;
+        push = mod(randi(3),3) - 1;
         for i = startpoint: endpoint
             steps(step + i) = (step + i - 1)*dt;
             t = steps(i);            
 %             F(step + i) = t*exp(-t/tau);
-            F(step + i) = fmax * push(step) * t*exp(-t/tau);
+            F(step + i) = fmax * push * t*exp(-t/tau);
 %             steps(step + i) = step * dt;
 %             F(step + i) = step+ i;
         end
@@ -33,7 +35,7 @@ for step = 1:(total_steps )
 %     step = step + 1;
 end
 
-disp(['steps(end): ' num2str(steps(end)) ', F(end): ' num2str(F(end)) ', max(F): ' num2str(max(F))]);
+disp(['steps(end): ' num2str(steps(end)) ', F(end): ' num2str(F(end)) ', F(' num2str(min(F)) ', ' num2str(max(F)) ')' ] );
 
 hold on;
 plot(steps,F);
